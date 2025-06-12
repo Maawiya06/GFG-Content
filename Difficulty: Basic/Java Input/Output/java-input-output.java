ce5ca9acfe08f0f055b0cb387a13318eecb27dee
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2025-06-12 18:32:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Input/Output/java-input-output.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+class Solution {
+    static void printIntString(String S, int N) {
+        // Print the string and integer with the required format
+        System.out.println("The input string :" + S);
+        System.out.println("The input integer :" + N);
+    }
+}

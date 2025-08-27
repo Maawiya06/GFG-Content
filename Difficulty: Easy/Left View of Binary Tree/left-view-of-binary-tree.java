3e3fd0215ced57004dd5f503aaee4ea609917214
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2025-08-27 13:25:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.java
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.java
@@ -1,4 +1,4 @@
-/* A Binary Tree node
+/*
 class Node
 {
     int data;
@@ -11,8 +11,7 @@ class Node
     }
 }*/
 class Solution {
-    // Function to return list containing elements of left view of binary tree.
-    void printleftview(Node root, int level, ArrayList<Integer> leftview){
+    public void printleftview(Node root, int level, ArrayList<Integer> leftview){
         if(root == null) return;
         
         if(level == leftview.size()){
@@ -22,11 +21,32 @@ class Solution {
         printleftview(root.left, level + 1, leftview);
         printleftview(root.right, level + 1, leftview);
     }
-    
     ArrayList<Integer> leftView(Node root) {
-        // code here
-        ArrayList<Integer> leftview = new ArrayList<>();
-        printleftview(root, 0, leftview);
-        return leftview;
+    //     // code here
+    //     ArrayList<Integer> ans = new ArrayList<>();
+    //     if(root == null) return ans;
+        
+    //     Queue<Node> queue = new LinkedList<>();
+    //     queue.add(root);
+        
+    //     while(!queue.isEmpty()){
+    //         int level = queue.size();
+            
+    //         for(int i = 0; i < level; i++){
+    //             Node curr = queue.poll();
+                
+    //             if(i == 0){
+    //                 ans.add(curr.data);
+    //             }
+    //             if(curr.left != null) queue.add(curr.left);
+    //             if(curr.right != null) queue.add(curr.right);
+    //         }
+    //     }
+        
+    //     return ans;
+    
+    ArrayList<Integer> leftview = new ArrayList<>();
+    printleftview(root, 0, leftview);
+    return leftview;
     }
 }

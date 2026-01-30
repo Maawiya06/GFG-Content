03fb00b95fd28816c3918b251148180aa60b91d1
Ameer maawiya <153859402+Maawiya06@users.noreply.github.com>
2026-01-30 12:25:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Generate IP Addresses/generate-ip-addresses.java
@@ -0,0 +1,28 @@
+/*complete the Function*/
+
+class Solution {
+    void solve(String s, int i, ArrayList<String> result, ArrayList<String> address){
+        if(address.size() == 4){
+            if(i == s.length()){
+                result.add(String.join(".", address));
+            }
+        }
+        else{
+            for(int j = i + 1; j <= i + 3 && j <= s.length(); j++){
+                String nextInt = s.substring(i, j);
+                if(Integer.parseInt(nextInt) <= 255 && (nextInt.equals("0") || !nextInt.startsWith("0"))){
+                    address.add(nextInt);
+                    solve(s, j, result, address);
+                    address.remove(address.size() - 1);
+                }
+            }
+        }
+    }
+    public ArrayList<String> generateIp(String s) {
+        // code here
+        ArrayList<String> result = new ArrayList<>();
+        ArrayList<String> address = new ArrayList<>();
+        solve(s, 0, result, address);
+        return result;
+    }
+}
